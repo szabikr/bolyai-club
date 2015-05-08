7713b5bdae775e7b5db9d348cfea62a5ef694a3f
szibill <n.szibill@gmail.com>
2015-05-08 18:08:32 +0300

    Keri munkat ad nekunk, hogy legyen mit torolni

--- BolyaiClubWindowsFormsApplication/BolyaiClubWindowsFormsApplication/View/MainForm.Designer.cs
+++ BolyaiClubWindowsFormsApplication/BolyaiClubWindowsFormsApplication/View/MainForm.Designer.cs
@@ -34,18 +34,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(65, 104);
+            this.label1.Location = new System.Drawing.Point(87, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.Size = new System.Drawing.Size(231, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hello Bolyai Club Application";
+            this.label1.Text = "Hello Bolyai Club Application(szibill)";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(379, 321);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
